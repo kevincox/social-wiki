484d121cb589ac1d2649dd693cc145cf4fc77637
Kevin Cox <kevincox@kevincox.ca>
2016-03-03 10:32:10 -0500

    rebasing of 123 commit

--- Gemfile
+++ Gemfile
@@ -11,7 +11,8 @@ gem 'rails', '~>4.2.5'
 gem 'sass-rails', '~>5.0'
 gem 'therubyracer', platforms: :ruby
 gem 'turbolinks'
-gem 'uglifier', '~>2.7'
+gem 'uglifier', '>= 1.3.0'
+gem 'bcrypt'
 
 group :development do
   gem 'byebug'
---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post = Post.find(params[:post_id])
+    @comment.user = current_user
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
+      else
+        format.html { render :show }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+private
+  def comment_params
+    params.require(:comment).permit(:contents)
+  end
+end
