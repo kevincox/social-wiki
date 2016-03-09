beb4888efc05d4d5749486c6ac52d10211486cda
ZachDFK <zacharie.gauthier@gmail.com>
2016-03-09 04:18:13 +0000

    made sure user param does not have :upvote, :downvote

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :password, :email, :upvote, :downvote)
+      params.require(:user).permit(:username, :password, :email, :votescore)
     end
 end
