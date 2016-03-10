12687d967f4a32d4862c4317fbee71f2cae6677b
Charles Wright <charliewright@live.ca>
2016-03-10 05:39:51 +0000

    add static home page and messing around with html styles

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
