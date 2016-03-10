634e13caf7dcfccd75bd229914f726d0978acab6
ZachDFK <zacharie.gauthier@gmail.com>
2016-03-10 06:15:04 +0000

    fixing test cases

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,29 +2,18 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
   test "should get login," do
-<<<<<<< HEAD
     get :login
-=======
-    get :login,
->>>>>>> login page and functionalbility
     assert_response :success
   end
 
   test "should get home," do
-<<<<<<< HEAD
     get :home
-=======
-    get :home,
->>>>>>> login page and functionalbility
     assert_response :success
   end
 
   test "should get profile," do
-<<<<<<< HEAD
+
     get :profile
-=======
-    get :profile,
->>>>>>> login page and functionalbility
     assert_response :success
   end
 
