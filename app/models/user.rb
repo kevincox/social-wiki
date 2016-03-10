a94fd13ab95003f64ff3f89754fccf9224e8bee8
ZachDFK <zacharie.gauthier@gmail.com>
2016-03-10 15:59:34 +0000

    space to tab fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ActiveRecord::Base
   has_many :comments
 =begin
 User attributes
-username: 	this is the username that the user goes by it can be used to log in. 
-	 		It showed be shown next to users posts and comments. S
-	 		Should be unique
-password: 	personal password used to log in 
-email: 		user's email addres it can be used to log in
+username:   this is the username that the user goes by it can be used to log in. 
+       It showed be shown next to users posts and comments. S
+       Should be unique
+password:   personal password used to log in 
+email:     user's email addres it can be used to log in
 =end
 
 
@@ -19,18 +19,18 @@ email: 		user's email addres it can be used to log in
   validates :password, confirmation: true, length: {in: 6..20}  
   validates :password_confirmation, presence: true
  
-  has_secure_password	  
+  has_secure_password    
   def self.authenticate_with_username_or_email(username_or_email="",login_password="")
-	if EMAIL_REGEX.match(username_or_email)
-	  user = User.find_by_email(username_or_email)
-	else
-	  user = User.find_by_username(username_or_email)
-	end
-	if user && user.authenticate(login_password)
-	  return user
-	else
-	  return nil
+  if EMAIL_REGEX.match(username_or_email)
+    user = User.find_by_email(username_or_email)
+  else
+    user = User.find_by_username(username_or_email)
+  end
+  if user && user.authenticate(login_password)
+    return user
+  else
+    return nil
     end
   end
-	
+  
 end
