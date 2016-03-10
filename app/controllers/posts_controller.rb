4982ab01856f120b737976afb67e0eda02cf5f36
ZachDFK <zacharie.gauthier@gmail.com>
2016-03-10 15:09:20 +0000

    last commit before milesstone 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-    @post.user =session[:user_id]
+    @post.user = User.find session[:user_id]
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
