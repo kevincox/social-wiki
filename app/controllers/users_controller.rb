8d97b5bbdc650c0952a295c70c177ebf994bd008
Kevin Cox <kevincox@kevincox.ca>
2016-03-21 18:29:18 -0400

    Fix syntax error and ensure all files are loaded in CI.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,14 +39,17 @@ class UsersController < ApplicationController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end 
-private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  end
+  
+  private
+  
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username,:password,:password_digest,:password_confirmation, :email)
-   end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:username,:password,:password_digest,:password_confirmation, :email)
+  end
 end
